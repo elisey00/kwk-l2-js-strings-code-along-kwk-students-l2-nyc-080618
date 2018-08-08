da945b5dfa654f09a7956de2f3dfd1c420eed1e0
elisey00 <eliseyoung000@gmail.com>
2018-08-08 14:27:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Write your code in this file!
 const currentUser=
 'Maya Young';
+
 const welcomeMessage=`Welcome to Klossybook, ${currentUser}!`;
+
+const excitedWelcomeMessage=welcomeMessage.toUpperCasse();
