613fda345c6061355a4f56f88b4c2c1b38cc127f
elisey00 <eliseyoung000@gmail.com>
2018-08-08 14:34:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const currentUser=
 
 const welcomeMessage=`Welcome to Klossybook, ${currentUser}!`;
 const excitedWelcomeMessage=welcomeMessage.toUpperCase();
-const shortGreeting=`Welcome, ${currentUser}!`;
+
+const shortGreeting=`Welcome, ${currentUser.slice(0,1}!`;
